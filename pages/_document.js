d9e8389fd3a1de73ae8bb1a770b674dfb1b0ba33
swapnil kumar <swapnilk0294@gmail.com>
2025-07-10 12:14:06 +0200

    refactor: update document structure and add manifest links for PWA support

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,18 @@
+// file templedtr/pages/_document.js:
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Link to the manifest file */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Meta tags for theme color and mobile support */}
+        <meta name="theme-color" content="#000000" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
